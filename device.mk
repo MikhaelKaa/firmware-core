bdfd3910651ba6bcd247bd54ffb2465cb214cc97
MikhaelKaa <unionfalse@gmail.com>
2026-06-24 03:20:03 +0300

    Адаптить device.mk под новую структуру с submodule
    - Заменить все пути mcu/$(MCU_PATH)/ на core/ (родительский Makefile
      включает device.mk из firmware/, поэтому все пути относительны к нему)
    - Добавить источники kernel (kernel.c, syscalls.c, sysmem.c, svccalls.c, drv_face.c)
    - Добавить ядро в секцию C includes (-Icore/kernel)
    - Сгруппировать C_SOURCES: kernel отдельно от drivers
    - Обновить LDSCRIPT и OPENOCD_MCU_CFG на core/vendor/...

--- device.mk
+++ device.mk
@@ -1,37 +1,44 @@
-#  stm32f407vg make
+#  stm32f407vg device config
 
 HEAP_SIZE  ?= 0x0400
 STACK_SIZE ?= 0x0800
 
-# C sources
-C_SOURCES += mcu/$(MCU_PATH)/drivers/rcc/rcc.c
-C_SOURCES += mcu/$(MCU_PATH)/drivers/rng/rng.c
-C_SOURCES += mcu/$(MCU_PATH)/drivers/uart/uart1.c
-C_SOURCES += mcu/$(MCU_PATH)/drivers/uart/uart2.c
-C_SOURCES += mcu/$(MCU_PATH)/drivers/mem/mem.c
-C_SOURCES += mcu/$(MCU_PATH)/drivers/rtc/rtc.c
-C_SOURCES += mcu/$(MCU_PATH)/drivers/rtc/rtc_time.c
-C_SOURCES += mcu/$(MCU_PATH)/drivers/led/pwm_led.c
-C_SOURCES += mcu/$(MCU_PATH)/drivers/w25q/w25q.c
-C_SOURCES += mcu/$(MCU_PATH)/drivers/usb_cdc/usb_cdc.c
-C_SOURCES += mcu/$(MCU_PATH)/drivers/usb_cdc/usb_desc.c
-C_SOURCES += mcu/$(MCU_PATH)/drivers/adc/adc.c
+# C sources (kernel)
+C_SOURCES += core/kernel/kernel.c
+C_SOURCES += core/kernel/syscalls.c
+C_SOURCES += core/kernel/sysmem.c
+C_SOURCES += core/kernel/svccalls.c
+C_SOURCES += core/kernel/drv_face.c
 
+# C sources (drivers)
+C_SOURCES += core/drivers/rcc/rcc.c
+C_SOURCES += core/drivers/rng/rng.c
+C_SOURCES += core/drivers/uart/uart1.c
+C_SOURCES += core/drivers/uart/uart2.c
+C_SOURCES += core/drivers/mem/mem.c
+C_SOURCES += core/drivers/rtc/rtc.c
+C_SOURCES += core/drivers/rtc/rtc_time.c
+C_SOURCES += core/drivers/led/pwm_led.c
+C_SOURCES += core/drivers/w25q/w25q.c
+C_SOURCES += core/drivers/usb_cdc/usb_cdc.c
+C_SOURCES += core/drivers/usb_cdc/usb_desc.c
+C_SOURCES += core/drivers/adc/adc.c
 
 # C includes
-C_INCLUDES += -Imcu/$(MCU_PATH)/vendor/CMSIS
-C_INCLUDES += -Imcu/$(MCU_PATH)/drivers/rcc
-C_INCLUDES += -Imcu/$(MCU_PATH)/drivers/rng
-C_INCLUDES += -Imcu/$(MCU_PATH)/drivers/uart
-C_INCLUDES += -Imcu/$(MCU_PATH)/drivers/mem
-C_INCLUDES += -Imcu/$(MCU_PATH)/drivers/rtc
-C_INCLUDES += -Imcu/$(MCU_PATH)/drivers/led
-C_INCLUDES += -Imcu/$(MCU_PATH)/drivers/w25q
-C_INCLUDES += -Imcu/$(MCU_PATH)/drivers/usb_cdc
-C_INCLUDES += -Imcu/$(MCU_PATH)/drivers/adc
+C_INCLUDES += -Icore/vendor/CMSIS
+C_INCLUDES += -Icore/kernel
+C_INCLUDES += -Icore/drivers/rcc
+C_INCLUDES += -Icore/drivers/rng
+C_INCLUDES += -Icore/drivers/uart
+C_INCLUDES += -Icore/drivers/mem
+C_INCLUDES += -Icore/drivers/rtc
+C_INCLUDES += -Icore/drivers/led
+C_INCLUDES += -Icore/drivers/w25q
+C_INCLUDES += -Icore/drivers/usb_cdc
+C_INCLUDES += -Icore/drivers/adc
 
 # ASM sources
-ASM_SOURCES +=  ./mcu/$(MCU_PATH)/vendor/startup/startup_stm32f407xx.s
+ASM_SOURCES +=  core/vendor/startup/startup_stm32f407xx.s
 # AS includes
 AS_INCLUDES += 
 
@@ -42,7 +49,6 @@ AS_DEFS +=
 C_DEFS += -DSTM32F407xx
 C_DEFS += -DBAREMETAL
 
-
 # CFLAGS
 # cpu
 CPU = -mcpu=cortex-m4
@@ -52,8 +58,8 @@ FPU = -mfpu=fpv4-sp-d16
 FLOAT-ABI = -mfloat-abi=hard
 
 # link script
-LDSCRIPT = ./mcu/$(MCU_PATH)/vendor/linker/STM32F407VGTx_FLASH.ld
-LDFLAGS += -Wl,--defsym=_Min_Heap_Size=$(HEAP_SIZE)
-LDFLAGS += -Wl,--defsym=_Min_Stack_Size=$(STACK_SIZE)
+LDSCRIPT = core/vendor/linker/STM32F407VGTx_FLASH.ld
+LDFLAGS += -Wl,--defsym=_Min_Heap_Size=\$(HEAP_SIZE)
+LDFLAGS += -Wl,--defsym=_Min_Stack_Size=\$(STACK_SIZE)
 
-OPENOCD_MCU_CFG ?= ./mcu/$(MCU_PATH)/vendor/openocd/stm32f4x.cfg
+OPENOCD_MCU_CFG ?= core/vendor/openocd/stm32f4x.cfg
